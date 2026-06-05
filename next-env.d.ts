c58f2eea9411562416bd5932df7f2623ada82ef3
liguangya4761 <liguangya@ztocc.com>
2026-06-06 00:11:56 +0800

    17

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,5 +1,12 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
 
-// NOTE: This file should not be edited
-// see https://nextjs.org/docs/app/building-your-application/configuring/typescript for more information.
+declare module "pdf-parse" {
+  export default function pdfParse(buf: Buffer): Promise<{
+    text: string; numpages: number; info: Record<string, unknown>;
+    metadata: Record<string, unknown>; version: string;
+  }>;
+}
+declare module "mammoth" {
+  export function extractRawText(opts: { arrayBuffer: ArrayBuffer }): Promise<{ value: string; messages: Array<{ message: string }> }>;
+}
